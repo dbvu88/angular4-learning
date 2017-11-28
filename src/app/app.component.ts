01155b6f8d69cc5539f73fa18f7736e8c31a798a
Duc <de.yd315@gmail.com>
2017-11-27 20:54:50 -0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,57 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  
+  template: `
+
+  <h1 *ngIf="!title; else mytemplate">I'm here</h1>
+
+  <ng-template #mytemplate><h1>I'm not here</h1></ng-template>
+
+  <div *ngIf="title; then truthtempl else fasletempl">
+  This will not appear.
+  </div>
+
+  <ng-template #truthtempl>I'm true<ng-template>
+  <ng-template #falsetempl>I'm false<ng-template>
+  
+  
+  <h1>
+  Inline HTML <br>
+  I {{ title }} <br>
+  {{ teams.baseball }}
+  </h1>
+  
+  <ul>
+    <li 
+    *ngFor="let team of sportTeams; 
+    let i = index;
+    let last = last
+    ">{{ i + 1 }}. {{ team.baseball }} <strong *ngIf="last">Hey, I'm last</strong></li>
+  </ul>
+    
+  <p>It certainly does!</p>
+  `,
   styleUrls: ['./app.component.css']
+   
+  /*
+  styles: [` h1 { color: green; } `]
+  */
+
 })
 export class AppComponent {
   title = 'app';
+
+  teams = {
+    baseball: 'Indians',
+    basketball: 'Cavaliers',
+    football: 'Browns'
+  }
+
+  sportTeams =[ 
+    { baseball: 'Indians'},
+    { baseball: 'Mets'},
+    { baseball: 'Yankees'},
+  ]
 }
